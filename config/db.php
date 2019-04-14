1af49900b5b904a498ca7afee0d29d6b66d15ad1
Leonid <torset1@mail.ru>
2019-04-15 00:24:11 +0500

    [fix] remove own pass the config db

--- config/db.php
+++ config/db.php
@@ -4,7 +4,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=yii2basic',
     'username' => 'root',
-    'password' => '55248888tT!?.,mn',
+    'password' => '',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
